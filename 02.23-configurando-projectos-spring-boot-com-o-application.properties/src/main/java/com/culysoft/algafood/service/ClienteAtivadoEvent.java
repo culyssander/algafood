576e3e1d502c170224c03748cb1a5e39d7b5bee8
Quitumba Ferreira <culyssandercentro@gmail.com>
2022-12-30 21:43:13 -0300

    configurando projectos Spring Boot com o aplication.properties

---
+++ 02.23-configurando-projectos-spring-boot-com-o-application.properties/src/main/java/com/culysoft/algafood/service/ClienteAtivadoEvent.java
@@ -0,0 +1,16 @@
+package com.culysoft.algafood.service;
+
+import com.culysoft.algafood.modelo.Cliente;
+
+public class ClienteAtivadoEvent {
+
+    private Cliente cliente;
+
+    public ClienteAtivadoEvent(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+}
