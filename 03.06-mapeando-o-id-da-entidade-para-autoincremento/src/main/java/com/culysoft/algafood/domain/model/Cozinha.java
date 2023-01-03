5ff875a42b94a0e2a733fb459a9ebcd824db9438
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-03 19:14:27 +0000

    mapeando o id da entidade para autoincremento

--- 03.06-mapeando-o-id-da-entidade-para-autoincremento/src/main/java/com/culysoft/algafood/domain/model/Cozinha.java
+++ 03.06-mapeando-o-id-da-entidade-para-autoincremento/src/main/java/com/culysoft/algafood/domain/model/Cozinha.java
@@ -1,11 +1,14 @@
 package com.culysoft.algafood.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Cozinha {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
--- 03.06-mapeando-o-id-da-entidade-para-autoincremento/src/main/java/com/culysoft/algafood/domain/model/Restaurante.java
+++ 03.06-mapeando-o-id-da-entidade-para-autoincremento/src/main/java/com/culysoft/algafood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.culysoft.algafood.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 public class Restaurante {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private BigDecimal taxaFrete;
