73f5ae1d0e76a9846a014f5ea89ee287396e1235
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-03 19:05:14 +0000

    criando as tabelas do banco de dados a partir das entidades

---
+++ 03.06-mapeando-o-id-da-entidade-para-autoincremento/src/main/java/com/culysoft/algafood/domain/model/Restaurante.java
@@ -0,0 +1,38 @@
+package com.culysoft.algafood.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Restaurante {
+
+    @Id
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+}
