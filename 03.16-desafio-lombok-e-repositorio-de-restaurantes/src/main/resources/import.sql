c3d61f49af83ca555b2c3429e0c59442d5ab5bf7
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-04 18:18:12 +0000

    desafio lombok e repositorio de restaurante

---
+++ 03.16-desafio-lombok-e-repositorio-de-restaurantes/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO cozinha(nome) VALUES ("Angolana");
+INSERT INTO cozinha(nome) VALUES ("Congolesa");
+
+INSERT INTO restaurante(nome, taxa_frete) VALUES("Bom de boa", 8.0);
+INSERT INTO restaurante(nome, taxa_frete) VALUES("Tudo de graca", 7.5);
