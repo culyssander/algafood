fccfeed308ad6d6a3ffa25547228f68ec20baa5a
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-09 20:39:34 -0300

    Corrigindo o Status HTTP para resource inexistente

---
+++ 04.21-Corrigindo-o-status-HTTP-para-resources-inexistente/src/main/java/com/culysoft/algafood/domain/model/Cozinha.java
@@ -0,0 +1,22 @@
+package com.culysoft.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
