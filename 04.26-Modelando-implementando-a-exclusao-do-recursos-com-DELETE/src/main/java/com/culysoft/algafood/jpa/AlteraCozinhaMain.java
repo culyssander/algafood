17b47bcbb436983520193541b3c1dd75841a733b
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-10 12:44:14 -0300

    modelando implementando a exclusao do recursos com DELETE

---
+++ 04.26-Modelando-implementando-a-exclusao-do-recursos-com-DELETE/src/main/java/com/culysoft/algafood/jpa/AlteraCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.culysoft.algafood.jpa;
+
+import com.culysoft.algafood.Application;
+import com.culysoft.algafood.domain.model.Cozinha;
+import com.culysoft.algafood.domain.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class AlteraCozinhaMain {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CozinhaRepository cadastroCozinha = context.getBean(CozinhaRepository.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Japonesa");
+
+        cadastroCozinha.salva(cozinha);
+
+        cadastroCozinha.todasCozinhas().forEach(System.out::println);
+    }
+}
