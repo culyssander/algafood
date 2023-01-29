6bbba21f0dba3681f8795152bfb9972b048a8aea
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-01-29 11:05:34 -0300

    Finalizando a actualizacao parcial com a api de reflections do Spring

---
+++ 04.34-Finalizando-a-atualizacao-parcial-com-a-api-de-reflections-do-spring/src/main/java/com/culysoft/algafood/domain/service/RestauranteService.java
@@ -0,0 +1,29 @@
+package com.culysoft.algafood.domain.service;
+
+import com.culysoft.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.culysoft.algafood.domain.model.Cozinha;
+import com.culysoft.algafood.domain.model.Restaurante;
+import com.culysoft.algafood.domain.repository.CozinhaRepository;
+import com.culysoft.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Restaurante salva(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+
+        Cozinha cozinha = cozinhaRepository.buscaPeloId(cozinhaId);
+
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.salva(restaurante);
+    }
+
+}
---
+++ 04.34-Finalizando-a-atualizacao-parcial-com-a-api-de-reflections-do-spring/src/main/java/com/culysoft/algafood/infrastracture/CozinhaRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.culysoft.algafood.infrastracture;
+
+import com.culysoft.algafood.domain.exception.EntidadeEmUsoException;
+import com.culysoft.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.culysoft.algafood.domain.model.Cozinha;
+import com.culysoft.algafood.domain.repository.CozinhaRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Cozinha> todasCozinhas() {
+        TypedQuery<Cozinha> typedQuery = entityManager.createQuery("FROM Cozinha c", Cozinha.class);
+
+        return typedQuery.getResultList();
+    }
+
+
+    @Override
+    public Cozinha buscaPeloId(Long id) {
+        Cozinha cozinha = entityManager.find(Cozinha.class, id);
+        if (cozinha == null)
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Nao existe cadastro de cozinha com codigo %d", id));
+        return cozinha;
+    }
+
+    @Transactional
+    @Override
+    public Cozinha salva(Cozinha cozinha) {
+        return entityManager.merge(cozinha);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Long id) {
+        Cozinha cozinha = buscaPeloId(id);
+
+            if (cozinha == null) {
+                throw new EmptyResultDataAccessException(1);
+            }
+
+        entityManager.remove(cozinha);
+    }
+}
